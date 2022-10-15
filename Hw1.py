0efad919e2afa441bc1ea3935d1a824fdb69a358
Ellina100 <dokalenkoel26@mayakvv.ru>
2022-10-15 16:05:08 +0300

    Добавила файл с домашкой

---
+++ Hw1.py
@@ -0,0 +1,53 @@
+'''from abc import ABC
+
+class Area(ABC):
+    def area(self):
+        pass
+class SquareArea(Area):
+    def __init__(self,a):
+        self.a = a
+    def area(self):
+        return self.a**2
+
+class RectangleArea(Area):
+    def __init__(self,b,a):
+        self.b = b
+        self.a = a
+    def area(self):
+        return self.a*self.b
+
+
+class TriangleArea(Area):
+    def __init__(self,c):
+        self.c = c
+        self.h = h
+    def area(self):
+        return self.c*self.c/2
+
+rectangle = RectangleArea(20,10)
+print(rectangle.area())'''
+
+
+class Counter:
+    def __init__(self,b):
+        self.__b = b
+
+    def getCounter(self):
+        return self.__b
+
+    def increment(self):
+        self.__b+=1
+
+    def decrement(self):
+        if self.__b <=0:
+            print("Ошибка")
+        else:
+            self.__b-=1
+
+
+
+counter = Counter(50)
+for x in range(1,52):
+    counter.decrement()
+    print(counter.getCounter())
+
